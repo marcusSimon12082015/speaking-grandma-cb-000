b44018867e79b601cdf2560f6218981111a7a67c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-21 15:27:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speak)
-  if speak == speak.upper
+  if speak == speak.upcase
     puts "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
